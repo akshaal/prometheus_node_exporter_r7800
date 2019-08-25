c3c05c943ad9e6ab934f9152a7d3d41e82fcf9ab
Evgeny Chukreev <Evgeny.Chukreev@gmail.com>
2019-08-25 18:24:38 +0200

    there is policy file in netgear's kernel

--- vendor/github.com/prometheus/procfs/sysfs/class_thermal.go
+++ vendor/github.com/prometheus/procfs/sysfs/class_thermal.go
@@ -30,7 +30,6 @@ type ClassThermalZoneStats struct {
 	Name    string  // The name of the zone from the directory structure.
 	Type    string  // The type of thermal zone.
 	Temp    uint64  // Temperature in millidegree Celsius.
-	Policy  string  // One of the various thermal governors used for a particular zone.
 	Mode    *bool   // Optional: One of the predefined values in [enabled, disabled].
 	Passive *uint64 // Optional: millidegrees Celsius. (0 for disabled, > 1000 for enabled+value)
 }
@@ -63,10 +62,7 @@ func parseClassThermalZone(zone string) (ClassThermalZoneStats, error) {
 	if err != nil {
 		return ClassThermalZoneStats{}, err
 	}
-	zonePolicy, err := util.SysReadFile(filepath.Join(zone, "policy"))
-	if err != nil {
-		return ClassThermalZoneStats{}, err
-	}
+  
 	zoneTemp, err := util.ReadUintFromFile(filepath.Join(zone, "temp"))
 	if err != nil {
 		return ClassThermalZoneStats{}, err
@@ -91,7 +87,6 @@ func parseClassThermalZone(zone string) (ClassThermalZoneStats, error) {
 
 	return ClassThermalZoneStats{
 		Type:    zoneType,
-		Policy:  zonePolicy,
 		Temp:    zoneTemp,
 		Mode:    zoneMode,
 		Passive: zonePassive,
